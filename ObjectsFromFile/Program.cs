a53c878a9c654628afaf9b421258a6f4d493dcb5
Opilane TTHK <opilane@tthk.ee>
2021-12-01 13:06:50 +0200

    first commit

---
+++ ObjectsFromFile/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+namespace ObjectsFromFile
+{
+    class Program
+    {
+        class Movie
+        {
+           public string title;
+           public string rating;
+           public string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string movie in GetDataFromFile())
+            {
+                string[] tempArray = movie.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+                Console.WriteLine($"Title: {newMovie.title}, rating: {newMovie.rating}, year: {newMovie.year}");
+            }
+
+            
+        }
+
+        public static string[] GetDataFromFile()
+        {
+            string filepath = @"C:\Users\opilane\Samples\Week14-ObjectsFromFile-file-main\movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+            return dataFromFile;
+        }
+
+
+        public static void DisplayArrayElements(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
